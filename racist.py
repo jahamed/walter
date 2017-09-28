b82df5fd8a98b84dd85167462f354ecd98c4ba14
Javed <jahamed@avvo.com>
2017-09-28 15:42:57 -0700

    detect # args"

--- racist.py
+++ racist.py
@@ -2,20 +2,23 @@ from keras.models import Sequential
 from keras.layers import Dense
 import numpy
 import pdb
+
 # fix random seed for reproducibility
-numpy.random.seed(7)
+numpy.random.seed(1337)
 
 # load pima indians dataset
-dataset = numpy.loadtxt("query_result.csv", delimiter=",")
-# dataset = numpy.loadtxt("query_result.csv", delimiter=",", usecols=[7])
-# split into input (X) and output (Y) variables
-X = dataset[:,0:9]
+dataset = numpy.loadtxt("query_result.csv", skiprows=1, delimiter=",")
 
-Y = dataset[:,9]
+# split into input (X) and output (Y) variables
+pdb.set_trace()
+num_arguments = dataset[0].size - 1
+
+X = dataset[:,0:num_arguments]
+Y = dataset[:,num_arguments]
 
 # create model
 model = Sequential()
-model.add(Dense(12, input_dim=9, activation='relu'))
+model.add(Dense(12, input_dim=num_arguments, activation='relu'))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
@@ -23,7 +26,7 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Fit the model
-model.fit(X, Y, epochs=50, batch_size=1000)
+model.fit(X, Y, epochs=5, batch_size=1000)
 
 # evaluate the model
 # scores = model.evaluate(X, Y)
@@ -35,7 +38,6 @@ predictions = model.predict(X)
 rounded = [round(x[0]) for x in predictions]
 # print(rounded)
 
-
 correct = 0
 for i in range(0, len(rounded)):
 	print("predicted: " + str(rounded[i]) + "  actual: " + str(Y[i]))
@@ -45,3 +47,5 @@ for i in range(0, len(rounded)):
 print("Right: " + str(correct))
 print("Wrong: " + str(len(Y) - correct))
 print("Accuracy: " + str(float(correct) / len(Y)))
+
+
