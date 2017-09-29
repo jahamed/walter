8b3a4c97e3729e8f77d51dfca781e3d562a8f4e1
Javed <jahamed@avvo.com>
2017-09-28 17:37:53 -0700

    off by 1 error

--- racist.py
+++ racist.py
@@ -3,27 +3,32 @@ from keras.layers import Dense
 import numpy
 import pdb
 
-# fix random seed for reproducibility
+# Fix random seed for reproducibility
 numpy.random.seed(1337)
 
-# load initial indians dataset
+# Load initial seed dataset
 print("Loading seed data, this might take a while...")
 dataset = numpy.loadtxt("query_result.csv", skiprows=1, delimiter=",")
 
-# split into input (X) and output (Y) variables
 # pdb.set_trace()
-num_arguments = dataset[0].size - 1
 
+# Split seed data into input (X) and output (Y) variables
+num_arguments = dataset.shape[1] - 1 # numpy array size
+print("# rows in seed dataset: {}").format(dataset.shape[0])
 X = dataset[:,0:num_arguments]
 Y = dataset[:,num_arguments]
 
-# dataset to preidct on
+# Dataset to predict on
 print("Loading data to predict, this might take a while...")
-dataset_to_predict = numpy.loadtxt("to_check.csv", skiprows=1, delimiter=",")
-X_predict = dataset[:,0:num_arguments]
-Y_predict = dataset[:,num_arguments]
+dataset_to_predict = numpy.loadtxt("to_check.csv", delimiter=",")
 
-# create model
+print("# rows in prediction dataset: {}").format(dataset_to_predict.shape[0])
+X_to_predict = dataset_to_predict[:,0:num_arguments]
+Y_to_predict = dataset_to_predict[:,num_arguments]
+num_rows_to_predict = dataset_to_predict.shape[0];
+print(num_rows_to_predict)
+
+# Create model
 model = Sequential()
 model.add(Dense(12, input_dim=num_arguments, activation='relu'))
 model.add(Dense(8, activation='relu'))
@@ -33,26 +38,31 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # Fit the model
-model.fit(X, Y, epochs=5, batch_size=100)
+model.fit(X, Y, epochs=1, batch_size=100)
 
-# evaluate the model
+# Evaluate the model
 # scores = model.evaluate(X, Y)
 # print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
-# calculate predictions
-predictions = model.predict(X_predict)
-# round predictions
+print("Time to calculate predictions :D")
+# Calculate predictions
+predictions = model.predict(X_to_predict)
+# pdb.set_trace()
+# Round predictions
 rounded = [round(x[0]) for x in predictions]
 # print(rounded)
 
 correct = 0
 for i in range(0, len(rounded)):
 	# print("predicted: " + str(rounded[i]) + "  actual: " + str(Y_predict[i]))
-	if rounded[i] == Y_predict[i]:
+	if rounded[i] == Y_to_predict[i]:
 		correct = correct + 1
 
+if len(rounded) != num_rows_to_predict:
+	print("# Arguments don't match up" + "len(rounded): " + str(len(rounded)) + "num args: " + str(num_rows_to_predict))
+
 print("Right: " + str(correct))
-print("Wrong: " + str(len(Y_predict) - correct))
-print("Accuracy: " + str(float(correct) / len(Y_predict)))
+print("Wrong: " + str(len(Y_to_predict) - correct))
+print("Accuracy: " + str(float(correct) / len(Y_to_predict)))
 
 
